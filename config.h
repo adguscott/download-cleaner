f5c0603d57437e3270fbbef71b51f91198acbced
Adam Guscott <adguscott@outlook.com>
2024-01-02 20:36:28 +0000

    removed name from config.h

--- config.h
+++ config.h
@@ -3,13 +3,13 @@
  * struct DirType found in <common.h>
  */
 
-static const char downloaddir[25] = "/home/adam/Downloads";
+static const char downloaddir[25] = "/home/user/Downloads";
 
 static const int dirtypes_length = 5;
 struct DirType dirtypes[5] = {
-    { "/home/adam/Pictures", { "png", "jpg", "jpeg", "gif", NULL } },
-    { "/home/adam/Music", { "mp3", "wav", "flac", "ogg", NULL } },
-    { "/home/adam/Videos", { "mp4", "mov", "avi", "mkv", NULL } },
-    { "/home/adam/Books", { "pdf", "epub", "mobi", "azw", NULL } },
-    { "/home/adam/Documents", { "docx", NULL, NULL, NULL, NULL } },
+    { "/home/user/Pictures", { "png", "jpg", "jpeg", "gif", NULL } },
+    { "/home/user/Music", { "mp3", "wav", "flac", "ogg", NULL } },
+    { "/home/user/Videos", { "mp4", "mov", "avi", "mkv", NULL } },
+    { "/home/user/Books", { "pdf", "epub", "mobi", "azw", NULL } },
+    { "/home/user/Documents", { "docx", NULL, NULL, NULL, NULL } },
 };
