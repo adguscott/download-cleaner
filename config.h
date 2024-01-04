60be9885d0ad622befd4539b07d5afa80d9899f8
adam <adguscott@outlook.com>
2024-01-04 19:05:10 +0000

    Modified config dirtypes to match changes

--- config.h
+++ config.h
@@ -7,9 +7,9 @@ static const char downloaddir[25] = "/home/user/Downloads";
 
 static const int dirtypes_length = 5;
 struct DirType dirtypes[5] = {
-    { "/home/user/Pictures", { "png", "jpg", "jpeg", "gif", NULL } },
-    { "/home/user/Music", { "mp3", "wav", "flac", "ogg", NULL } },
-    { "/home/user/Videos", { "mp4", "mov", "avi", "mkv", NULL } },
-    { "/home/user/Books", { "pdf", "epub", "mobi", "azw", NULL } },
-    { "/home/user/Documents", { "docx", NULL, NULL, NULL, NULL } },
+    { "/home/user/Pictures", "png,jpg,jpeg,gif" },
+    { "/home/user/Music", "mp3,wav,flac,ogg" },
+    { "/home/user/Videos", "mp4,mov,avi,mkv" },
+    { "/home/user/Books", "pdf,epub,mobi,azw" },
+    { "/home/user/Documents", "docx" },
 };
